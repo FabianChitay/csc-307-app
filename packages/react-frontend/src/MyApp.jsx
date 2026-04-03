f2bdc0c0494a43f0f8ec709de0159fca63879081
Fabian Chitay <fchitay@calpoly.edu>
2026-04-03 07:35:42 -0700

    lab1A part 4 complete

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -2,12 +2,31 @@
 import React from "react";
 import Table from "./Table";
 
+const characters = [
+  {
+    name: "Charlie",
+    job: "Janitor"
+  },
+  {
+    name: "Mac",
+    job: "Bouncer"
+  },
+  {
+    name: "Dee",
+    job: "Aspring actress"
+  },
+  {
+    name: "Dennis",
+    job: "Bartender"
+  }
+];
+
 function MyApp() {
-  return (
+    return (
     <div className="container">
-      <Table />
+        <Table characterData={characters} />
     </div>
-  );
+    );
 }
 
 export default MyApp;
